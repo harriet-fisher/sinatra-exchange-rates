259ee2080afc6906dfdb3eae417af0d2b5db6ebe
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-10-24 21:10:11 +0000

    done

--- app.rb
+++ app.rb
@@ -2,41 +2,32 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 require "json"
-
-API_URL = "https://api.exchangerate.host/"
 API_KEY = ENV.fetch('EXCHANGE_RATE_KEY')
 
 get '/' do
-  response = HTTP.get("#{API_URL}/list?access_key=#{API_KEY}")
-
-  begin
-    data = JSON.parse(response.to_s)
-    @currencies = data["currencies"].keys
-    erb(:currency_list)
-  end
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  data = JSON.parse(raw_data_string)
+  @currencies = data["currencies"].keys
+  erb(:currency_list)
 end
 
 get '/:currency' do
-  response = HTTP.get("#{API_URL}/list?access_key=#{API_KEY}")
-
-  begin
-    data = JSON.parse(response.to_s)
-    @primary_currency = params[:currency]
-    @currencies = data["currencies"].keys
-    erb(:conversion_options)
-  end
+  @original_currency = params.fetch("currency")
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw = HTTP.get(api_url)
+  data = JSON.parse(raw.to_s)
+  @currencies = data["currencies"].keys
+  erb(:conversion_options)
 end
 
 get '/:currency/:target_currency' do
-  from_currency = params[:currency]
-  to_currency = params[:target_currency]
-  response = HTTP.get("#{API_URL}/convert?access_key=#{API_KEY}&from=#{from_currency}&to=#{to_currency}&amount=1")
-
-  begin
-    data = JSON.parse(response.to_s)
-    @result = data["result"]
-    @from_currency = from_currency
-    @to_currency = to_currency
-    erb(:convert_result)
-  end
+  @original_currency = params.fetch("currency")
+  @destination_currency = params.fetch("target_currency")
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  response = HTTP.get(api_url)
+  data = JSON.parse(response.to_s)
+  @result = data["result"]
+  erb(:convert_result)
 end
