03338063943a13ed9e634f45d86e5c4fd9400568
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-10-24 19:45:28 +0000

    API

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
 
+api_url = api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
